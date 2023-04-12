ac54760053d9aa7f346b2a44f3e60a75db68297f
Mauro Garro <maurogarro73@gmail.com>
2023-04-12 18:17:14 -0300

    finalizado

--- desafio1.js
+++ desafio1.js
@@ -14,15 +14,37 @@ class ProductManager {
       stock,
     };
 
-    const productFound = this.products.some((item) => item.code == code);
-    if (productFound) {
-      console.log("El producto ya existe");
+    if (title == undefined || description == undefined || price == undefined || thumbnail == undefined || code == undefined || stock == undefined) {
+      console.log('Debe completar todos los campos');
     } else {
-      this.products.push(product);
+      const productFound = this.products.some((item) => item.code == code);
+      if (productFound) {
+        console.log('El producto ya existe');
+      } else {
+        this.products.push(product);
+      }
     }
   }
 
   getProducts() {
     console.log(this.products);
   }
+
+  getProductById(id) {
+    const productFound = this.products.find((item) => item.id == id);
+    if (productFound) {
+      console.log('El producto encontrado es ', productFound);
+    } else {
+      console.log('Not found');
+    }
+  }
 }
+
+const product1 = new ProductManager();
+/* product1.getProducts(); */
+
+product1.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
+product1.addProduct('producto prueba 2', 'Este es un producto prueba', 200, 'Sin imagen', 'abc124', 25);
+product1.addProduct('producto prueba 3', 'Este es un producto prueba', 200, 'Sin imagen', 'abc125', 25);
+product1.getProducts();
+product1.getProductById(1);
